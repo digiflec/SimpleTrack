ef62eabbfa79a45da44e3ea6b65ab35b0fd680a7
krish-digiflec <krish@digiflec.com>
2024-09-30 13:43:08 +0100

    updating setup.py to proparly install mot_3d as dist_package

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from distutils.core import setup
-
+from setuptools import setup, find_packages
 
 setup(
     name='mot_3d',
     version='0.1',
-    packages=['mot_3d']
+    description='Multi-object Tracking in 3D Space',
+    author='Krish-Digiflec',
+    author_email='krish@digiflec.com',
+    url='https://github.com/digiflec/SimpleTrack.git',  # Replace with your repository URL
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
 )
